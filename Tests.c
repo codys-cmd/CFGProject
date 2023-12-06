556815461c3b6923948688322875f06747a66bf2
codys-cmd <codsav03@outlook.com>
2023-12-06 16:13:58 -0500

    Added Tests target and executable

---
+++ Tests.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Interpreter.h"
+
+int main() {
+
+    SymbolTable* table = (SymbolTable*) malloc(sizeof(SymbolTable));
+    table->length = 0;
+    table->symbolTokens[0] = NULL;
+
+    int testsPassed = 0;
+
+    printf("Starting Test 1...\n");
+    cfg_interpretStatements(
+        "a = 5;",
+        table
+    );
+
+    if (
+        table->length == 1 &&
+        table->types[0] == Integer &&
+        table->values[0].integer == 5
+    ) {
+        printf("Test #1 Passed!\n");
+        testsPassed++;
+    } 
+
+    printf("Starting Test 2...\n");
+    cfg_interpretStatements(
+        "x = 5; if (x < 10) { y = 20; } else { y = 30; }",
+        table
+    );
+
+    if (
+        table->length == 3 &&
+        table->types[2] == Integer &&
+        table->values[2].integer == 20
+    ) {
+        printf("Test #2 Passed!\n");
+        testsPassed++;
+    } 
+
+    printf("Starting Test 3...\n");
+    cfg_interpretStatements(
+        "i = 0; while (i < 5) { i = i + 1; }",
+        table
+    );
+
+    if (
+        table->length == 4 &&
+        table->types[3] == Integer &&
+        table->values[3].integer == 5
+    ) {
+        printf("Test #3 Passed!\n");
+        testsPassed++;
+    } 
+
+    printf("Starting Test 4...\n");
+    cfg_interpretStatements(
+        "a = -1; if (a == 1) { b = 2; } else { b = 3; }",
+        table
+    );
+
+    if (
+        table->length == 5 &&
+        table->types[4] == Integer &&
+        table->values[4].integer == 3
+    ) {
+        printf("Test #4 Passed!\n");
+        testsPassed++;
+    } 
+
+    printf("Starting Test 5...\n");
+    cfg_interpretStatements(
+        "c = (true || false) && (false && true);",
+        table
+    );
+
+    if (
+        table->length == 6 &&
+        table->types[5] == Boolean &&
+        table->values[5].boolean == False
+    ) {
+        printf("Test #5 Passed!\n");
+        testsPassed++;
+    } 
+
+    printf("%d / 5 Tests Passed. Press enter to close.", testsPassed);
+    char input;
+    scanf("%c", &input);
+
+}
